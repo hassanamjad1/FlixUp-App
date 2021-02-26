e491a9e63932a0d47c6fae2bbe5ac2442e2103e2
Hassan Amjad <amjadhassan535@gmail.com>
2021-02-25 22:58:04 -0500

    Adding

---
+++ flixup/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixup
+//
+//  Created by Hassan Amjad on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
